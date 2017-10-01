b205c638a1b169581a9ebece7a2aee7a674b95a7
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-01 11:02:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@
 def line (arr)
   if(arr.size == 0)
      puts "The line is currently empty."
-  elsif(arr.size >= 1)
+  else
     message = "The line is currently: "
     arr.each_with_index { |customer, index| puts message+= "#{index+1}. #{customer}" }
   end
