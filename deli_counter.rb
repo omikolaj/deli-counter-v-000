3db5c65f71203af00cb97c7f9d84764065fc5619
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-01 10:45:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,8 @@ def line (katz_deli)
   if(katz_deli.size == 0)
      puts "The line is currently empty."
   else
-
+    message = "The line is currently: "
+    katz_deli.each_with_index {|customer, index| message+= "#{index+1}. #{customer}" }
   end
 end
 
