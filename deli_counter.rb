c00d920d55e77ceb84cf724ad4335e900813aa10
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-01 11:10:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,8 @@ def line (arr)
   if(arr.size == 0)
      puts "The line is currently empty."
   else
-    message = "The line is currently: "
-    arr.each_with_index { |customer, index| message+= "#{index+1}. #{customer}" }
+    message = "The line is currently:"
+    arr.each_with_index { |customer, index| message+= " #{index+1}. #{customer}" }
     puts message
   end
 
