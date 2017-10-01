5a4cee38ba528f0b63e6c0d3095019c931b915e9
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-01 10:33:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,13 @@
 # Write your code here.
 def line (katz_deli)
-
+  if(katz_deli.size == 0)
+    "The line is currently empty"
+  else
+    counter = 1
+    until (counter == katz_deli.size)
+    "The line is currently: #{counter}. #{katz_deli[counter-1]}"
+    counter += 1
+    
 end
 
 def take_a_number(katz_deli, customer)
