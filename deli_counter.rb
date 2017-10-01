44b6cb66b483eab70311719f9fca6ff30965f14e
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-01 10:35:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,11 +3,7 @@ def line (katz_deli)
   if(katz_deli.size == 0)
     "The line is currently empty."
   else
-    counter = 1
-    until (counter == katz_deli.size)
-    "The line is currently: #{counter}. #{katz_deli[counter-1]}"
-    counter += 1
-    end
+    
   end
 end
 
