91561f586ffb8c0815c9210e1f5157f3befab8a3
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-01 11:29:04 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ def now_serving (arr)
   if(arr.size == 0)
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{arr.shift}"
+    puts "Currently serving #{arr.shift}."
 
   end
 
